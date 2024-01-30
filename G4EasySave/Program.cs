5d27b9a643d057e45ac4d052f387a0c20355d27b
Faik MEHMETI <faik.mehmeti67@gmail.com>
2024-01-30 15:12:00 +0100

    Re initialisation du projet EasySave

---
+++ G4EasySave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace G4EasySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initialisation projet EasySave");
+        }
+    }
+}

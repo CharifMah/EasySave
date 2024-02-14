c06b1da2476f9452b0412dde177fec1578fbb6f1
CharifMah <charif25430@hotmail.fr>
2024-02-14 18:28:43 +0100

    Ajout du guide d'utilisation

--- EasySave-G4-FISA-24/EasySave/Views/JobView.cs
+++ EasySave-G4-FISA-24/EasySave/Views/JobView.cs
@@ -213,7 +213,7 @@ namespace EasySave.Views
                     _JobVm.LoadJobs();
                     break;
                 case "1":
-                    _JobVm.LoadJobs(false, ConsoleExtention.ReadFile("Choisir le fichier de configuration", new Regex("^.*\\.(json | JSON)$"), Path.GetDirectoryName(Models.Settings.Instance.JobConfigFolderPath)));
+                    _JobVm.LoadJobs(false, ConsoleExtention.ReadFile("Choisir le fichier de configuration", new Regex("^.*\\.(json | JSON)$"), Path.GetDirectoryName(Models.CSettings.Instance.JobConfigFolderPath)));
                     if (_JobVm.JobManager.Jobs.Count > 0)
                         ConsoleExtention.WriteLineSucces($"{_JobVm.JobManager.Name} Loaded");
                     else
